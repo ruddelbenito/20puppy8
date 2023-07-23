de3ae090bb8e19a78ec7943ea518e2f322a15f4c
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-07-23 16:16:53 -0600

    initialized grid values

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+let firstRow = [0, 0, 0, 0]
+let secondRow = [0, 0, 0, 0]
+let thirdRow = [0, 0, 0, 0]
+let fourthRow = [0, 0, 0, 0]
+
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <h1>20puppy8</h1>
     </main>
   )
 }
