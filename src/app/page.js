0ba78fed77a0b4686a8dad139cbadfe6408b6a12
Russel Benito <59615148+9russelb@users.noreply.github.com>
2023-07-25 13:26:38 -0600

    Removed unused import Added comments for future functions

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 let firstRow = [0, 0, 0, 0]
 let secondRow = [0, 0, 0, 0]
 let thirdRow = [0, 0, 0, 0]
 let fourthRow = [0, 0, 0, 0]
+
+// function to set a board space to a value
+
+// function to randomly set an empty board space to a value of 2 or 4
+
+// function to initialize board (determine first two tiles and their values)
+
+// function to merge two spaces if they are adjacent and the same value
+
+// function to shift spaces up
+
+// function to shift spaces down
+
+// function to shift spaces left
+
+// function to shift spaces right
 
 export default function Home() {
   return (
