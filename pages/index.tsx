8dc08bf4aed9d6ab7edc23c58044b48ae873a3bd
Russel Benito <59615148+9russelb@users.noreply.github.com>
2024-04-30 20:00:32 -0600

    Added styling to tiles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
 import Board from "@/components/board";
 import Head from "next/head";
+import styles from "@/styles/index.module.css";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.twentypuppyeight}>
       <Head>
         <title>20puppy8</title>
         <meta name="description" content="2048, but with puppies" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header>
+        <h1>20puppy8</h1>
+      </header>
       <main>
         <Board />
       </main>
-    </>
+      {/* Footer */}
+      {/* <footer>stuf here</footer> */}
+    </div>
   );
 }
